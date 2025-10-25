c2077539b90c774caf8b0b1c14b9c0d105f72496
Maxim <maximkutkov@gmail.com>
2025-10-25 10:55:51 +0000

    microfix

--- src/main.cpp
+++ src/main.cpp
@@ -12,53 +12,64 @@ int main() {
     std::cin >> n;
 
     for (int i = 0; i < n; ++i) {
-        std::cout << "\nВыберите тип фигуры (1-Rectangle, 2-Trapezoid, 3-Rhombus): ";
+        std::cout << "\nФигура №" << (i + 1) << '\n';
+        std::cout << "Выберите тип (1 - прямоугольник, 2 - трапеция, 3 - ромб): ";
+
         int type;
         std::cin >> type;
 
-        if (type == 1) {
-            double w, h;
-            std::cout << "Введите ширину и высоту прямоугольника: ";
-            std::cin >> w >> h;
-            figures.add(new Rectangle(w, h, "Rectangle"));
-        } 
-        else if (type == 2) {
-            double a, b, h;
-            std::cout << "Введите основания и высоту трапеции: ";
-            std::cin >> a >> b >> h;
-            figures.add(new Trapezoid(a, b, h, "Trapezoid"));
-        } 
-        else if (type == 3) {
-            double d1, d2;
-            std::cout << "Введите диагонали ромба: ";
-            std::cin >> d1 >> d2;
-            figures.add(new Rhombus(d1, d2, "Rhombus"));
-        } 
-        else {
-            std::cout << "Неверный тип фигуры.\n";
-            --i;
+        switch (type) {
+            case 1: {
+                double w, h;
+                std::cout << "Введите ширину и высоту прямоугольника: ";
+                std::cin >> w >> h;
+                figures.add(new Rectangle(w, h, "Прямоугольник"));
+                break;
+            }
+
+            case 2: {
+                double a, b, h;
+                std::cout << "Введите нижнее основание, верхнее основание и высоту трапеции: ";
+                std::cin >> a >> b >> h;
+                figures.add(new Trapezoid(a, b, h, "Трапеция"));
+                break;
+            }
+
+            case 3: {
+                double d1, d2;
+                std::cout << "Введите длины диагоналей ромба: ";
+                std::cin >> d1 >> d2;
+                figures.add(new Rhombus(d1, d2, "Ромб"));
+                break;
+            }
+
+            default:
+                std::cout << "Неизвестный тип фигуры, попробуйте снова.\n";
+                --i; // Повторяем ввод
+                break;
         }
     }
 
-    std::cout << "\nСписок фигур:\n";
+    std::cout << "\nСписок всех фигур:\n";
     figures.printAll();
 
-    std::cout << "\nСуммарная площадь всех фигур: " << figures.totalArea() << "\n";
+    std::cout << "\nСуммарная площадь: " << figures.totalArea() << "\n";
 
-    std::cout << "\nВведите индекс фигуры для удаления: ";
+    std::cout << "\nВведите номер фигуры для удаления: ";
     int index;
     std::cin >> index;
+
     try {
         figures.remove(index);
         std::cout << "Фигура удалена.\n";
     } catch (const std::exception& e) {
-        std::cout << "Ошибка: " << e.what() << "\n";
+        std::cout << "Ошибка при удалении: " << e.what() << "\n";
     }
 
-    std::cout << "\nОбновленный список фигур:\n";
+    std::cout << "\nОставшиеся фигуры:\n";
     figures.printAll();
 
-    std::cout << "\nОбновленная суммарная площадь: " << figures.totalArea() << "\n";
+    std::cout << "\nИтоговая площадь после удаления: " << figures.totalArea() << "\n";
 
     return 0;
 }
